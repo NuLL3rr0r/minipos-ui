3536a2bf7bd1933c8cfdc1df66648a7aa2adb509
M.S. Babaei <info@babaei.net>
2014-07-21 18:23:51 +0430

    Added check for device connectivity

--- UiEngine.cpp
+++ UiEngine.cpp
@@ -1,34 +1,54 @@
 #include <QDebug>
 #include <QQmlContext>
+#include "make_unique.hpp"
 #include "uiengine.hpp"
 #if defined(Q_OS_ANDROID)
 #include "Android.hpp"
 #endif // defined(Q_OS_ANDROID)
 #include "Pool.hpp"
+#include "Pos.hpp"
 
 using namespace MiniPos;
 
 struct UiEngine::Impl
 {
+private:
+   UiEngine *m_parent;
 
+public:
+   Impl(UiEngine *parent);
+
+public:
+    void OnHeadSetStateChanged(const Pos::HeadSetState &state);
+
+    void InitializeEvents();
 };
 
 UiEngine::UiEngine(QObject *parent) :
-    QQmlApplicationEngine(parent)
+    QQmlApplicationEngine(parent),
+    m_pimpl(std::make_unique<UiEngine::Impl>(this))
 {
     this->rootContext()->setContextProperty("uiEngine", this);
+
+    m_pimpl->InitializeEvents();
 }
 
 UiEngine::UiEngine(const QUrl &url, QObject *parent) :
-    QQmlApplicationEngine(url, parent)
+    QQmlApplicationEngine(url, parent),
+    m_pimpl(std::make_unique<UiEngine::Impl>(this))
 {
     this->rootContext()->setContextProperty("uiEngine", this);
+
+    m_pimpl->InitializeEvents();
 }
 
 UiEngine::UiEngine(const QString &filePath, QObject *parent) :
-    QQmlApplicationEngine(filePath, parent)
+    QQmlApplicationEngine(filePath, parent),
+    m_pimpl(std::make_unique<UiEngine::Impl>(this))
 {
     this->rootContext()->setContextProperty("uiEngine", this);
+
+    m_pimpl->InitializeEvents();
 }
 
 UiEngine::~UiEngine() = default;
@@ -53,5 +73,42 @@ bool UiEngine::showToast(const QString &text, const int duration) const
     qDebug() << text << duration;
     return true;
 #endif // defined(Q_OS_ANDROID)
+}
+
+UiEngine::Impl::Impl(UiEngine *parent) :
+    m_parent(parent)
+{
+
+}
+
+void UiEngine::Impl::OnHeadSetStateChanged(const Pos::HeadSetState &state)
+{
+    switch (state) {
+    case Pos::HeadSetState::DEVICE_IN:
+        m_parent->showToast("Device In!");
+        break;
+    case Pos::HeadSetState::DEVICE_OUT:
+        m_parent->showToast("Device Out!");
+        break;
+    case Pos::HeadSetState::DEVICE_CHECKED:
+        m_parent->showToast("Device Checked!");
+        break;
+    case Pos::HeadSetState::DEVICE_CHECKED_TIMEOUT:
+        m_parent->showToast("Device Device Checked Timeout!");
+        break;
+    default:
+        break;
+    }
+}
+
+void UiEngine::Impl::InitializeEvents()
+{
+    Pool::Pos()->HeadSetStateChanged().connect(
+                std::bind(
+                    &UiEngine::Impl::OnHeadSetStateChanged,
+                    this,
+                    std::placeholders::_1
+                    )
+                );
 }
 
